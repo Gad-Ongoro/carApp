d7fb7cfaa2bb0f9cef314356af1d16579e0d1319
Gad-Ongoro <gadongoro1@gmail.com>
2024-01-16 21:23:25 +0300

    Flask MVC

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
-
-    db.init_app(app)
-
+    
+    migrate = Migrate(app, db)
+    
     from .routes import bp
     app.register_blueprint(bp)
+    
+    db.init_app(app)
     
     with app.app_context():
         db.create_all()
--- app/models/sales_model.py
+++ app/models/sales_model.py
@@ -3,7 +3,7 @@ from app import db
 class Sales(db.Model):
     __tablename__ = "sales"    
     sale_id = db.Column(db.Integer, primary_key = True)
-    payment_method = db.Column(db.Column)
+    payment_method = db.Column(db.String)
     amount = db.Column(db.Integer)
     status = db.Column(db.String(100))
     car_id = db.Column(db.Integer, db.ForeignKey('car.id'))
